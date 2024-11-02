5055035cf1e11c2f20123e2b8ddd16d1115395dd
Chrisjuunor <chinonsoigweic@gmail.com>
2024-11-02 01:56:46 +0100

    add new booking

--- server/src/routes/bookings.controller.js
+++ server/src/routes/bookings.controller.js
@@ -1,9 +1,30 @@
-const { getAllBookings } = require("../models/bookings.model");
+const { getAllBookings, addNewBooking } = require("../models/bookings.model");
 
 function httpGetAllBookings(req, res) {
   return res.status(200).json(getAllBookings());
 }
 
+function httpAddNewBooking(req, res) {
+  const book = req.body;
+
+  if (!book.vehicle || !book.destination || !book.tripDate) {
+    return res.status(400).json({
+      error: "Missing booking requirements!",
+    });
+  }
+
+  book.tripDate = new Date(book.tripDate);
+  if (isNaN(book.tripDate)) {
+    res.status(400).json({
+      error: "invalid date!",
+    });
+  }
+
+  addNewBooking(book);
+  return res.status(201).json(launch);
+}
+
 module.exports = {
   httpGetAllBookings,
+  httpAddNewBooking,
 };
--- server/src/routes/bookings.router.js
+++ server/src/routes/bookings.router.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
-const {httpGetAllBookings} = require("./bookings.controller");
+const {
+  httpGetAllBookings,
+  httpAddNewBooking,
+} = require("./bookings.controller");
 
 const bookingsRouter = express.Router();
 
 bookingsRouter.get("/", httpGetAllBookings);
+bookingsRouter.post("/", httpAddNewBooking);
 
-module.exports = { bookingsRouter, }
+module.exports = { bookingsRouter };
